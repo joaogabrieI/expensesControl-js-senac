1ec9815cad80f69257749a62fd75c674952d037c
joaogabriei <joaonalves1@gmail.com>
2026-09-06 21:40:05 -0300

    feat: select expense tracker dom elements

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+// Referências do DOM
+const modalToggle = document.getElementById('modal-toggle');
+const formNovoLancamento = document.getElementById('form-novo-lancamento');
+const inputDescricao = document.getElementById('descricao');
+const inputValor = document.getElementById('valor');
+const selectCategoria = document.getElementById('categoria');
+const radioReceita = document.getElementById('tipo-receita');
+const radioDespesa = document.getElementById('tipo-despesa');
+const btnSalvar = document.getElementById('btn-salvar-lancamento');
+
+const listaLancamentos = document.getElementById('lista-lancamentos');
+const listaCategoriasSidebar = document.getElementById('lista-categorias-sidebar');
+const listaChipsFiltro = document.getElementById('lista-chips-filtro');
